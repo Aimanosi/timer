dcbdd3719680b9d0b82006702c2dfb21e3aabc35
nasiru hammed aimanosi <nasaces360@gmail.com>
2022-10-23 23:31:10 +0100

    timer countdown

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+const countDate = new Date("Oct 28 2022 00:00:00").getTime();
+
+const countDown = () => {
+  const now = new Date().getTime();
+  const countDownTime = countDate - now;
+  if (countDownTime <= 1000) {
+    document.querySelector(".text-time").style.display = "none";
+    document.querySelector(".expired").textContent = "HAPPY BIRTHDAY MOPEL!🎉";
+  }
+  // if (countDownTime <= 1000) {
+  //   document.querySelector(".text-time").style.display = "none";
+  //   document.querySelector(".expired").textContent = "TIME HAS EXPIRED!";
+  // }
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  let textDay = Math.floor(countDownTime / day);
+  let textHour = Math.floor((countDownTime % day) / hour);
+  let textMinute = Math.floor((countDownTime % hour) / minute);
+  let textSecond = Math.floor((countDownTime % minute) / second);
+
+  document.querySelector(".day").textContent =
+    textDay < 10 ? `0${textDay}` : `${textDay}`;
+  document.querySelector(".hour").textContent =
+    textHour < 10 ? `0${textHour}` : `${textHour}`;
+  document.querySelector(".minute").textContent =
+    textMinute < 10 ? `0${textMinute}` : `${textMinute}`;
+  document.querySelector(".second").textContent =
+    textSecond < 10 ? `0${textSecond}` : `${textSecond}`;
+};
+setInterval(countDown, 1000);
